60b923c2b260719e8379c8e903a58d9d566b2ecf
Alpinoid <valentin.avramko@gmail.com>
2015-03-11 17:13:21 +0300

    [BI-4] Процедура обновления талицы фактов продаж ISIS
    Процедура [update_SalesISIS].

--- Процедуры. Обновление таблиц фактов/dbo.update_SalesISIS.sql
+++ Процедуры. Обновление таблиц фактов/dbo.update_SalesISIS.sql
@@ -16,9 +16,9 @@ BEGIN
 	MERGE INTO dbo.t_SalesISIS AS ReportingTable
 	USING (	
 			SELECT
-				CAST(RegSales._Период AS date) AS TransactionDate		-- Дата операции
+				CAST(ISISTable.DUE_PERD AS int) AS TransactionDateID	-- Дата операции
 				,Documents.ID AS DocumentID								-- ID документа
-				,RegSales.НомерСтроки AS DocumentRow					-- Номер строки документа
+				,ISISTable._row_code AS DocumentRow						-- Номер строки документа
 				,SalesDocuments.ID AS SalesDocumentID					-- ID документа продажи
 				,TransactionsType.ID AS TransactionTypeID				-- ID типа операции
 				,CASE
@@ -40,8 +40,8 @@ BEGIN
 					INNER JOIN (	SELECT TOP 1
 										TradeChanel.ISISКанал
 									FROM [uas_central].dbo.РегистрСведений_ПериодическиеРеквизитыТочекДоставки AS TradeChanel
-									WHERE TradeChanel.ТочкаДоставки = RegSales.ТочкаДоставки
-											AND TradeChanel._Период <= RegSales._Период
+									WHERE TradeChanel.ТочкаДоставки = ISISTable._client_id_new
+											AND TradeChanel._Период <= CAST(ISISTable.DUE_PERD AS date)
 									ORDER BY TradeChanel._Период DESC) AS Реквизиты ON Реквизиты.ISISКанал = UID_1C
 				) AS TradeChanelID										-- ID канала торговли
 				,Goods.ID AS GoodID										-- ID номенклатуры
@@ -52,22 +52,33 @@ BEGIN
 										CSKUStates.ВидИнициативы AS InitiativesType
 									FROM [uas_central].dbo.РегистрСведений_СтатусыКодовДистрибьюции AS CSKUStates
 									INNER JOIN [uas_central].dbo.РегистрСведений_КодыCSKU AS Good ON Good.CSKU = CSKUStates.CSKU
-																								AND Good.Номенклатура = RegSales.Номенклатура
+																								AND Good.Номенклатура = ISISTable._tovar_id_new
 									INNER JOIN (	SELECT TOP 1
 														TradeChanel.ISISКанал
 													FROM [uas_central].dbo.РегистрСведений_ПериодическиеРеквизитыТочекДоставки AS TradeChanel
-													WHERE TradeChanel.ТочкаДоставки = RegSales.ТочкаДоставки
-															AND TradeChanel._Период <= RegSales._Период
+													WHERE TradeChanel.ТочкаДоставки = ISISTable._client_id_new
+															AND TradeChanel._Период <= CAST(ISISTable.DUE_PERD AS date)
 													ORDER BY TradeChanel._Период DESC
 												) AS Реквизиты ON Реквизиты.ISISКанал = CSKUStates.КаналПродаж
-									WHERE CSKUStates._Период <= RegSales._Период
+									WHERE CSKUStates._Период <= CAST(ISISTable.DUE_PERD AS date)
 										AND CSKUStates.Активна = 0x01
 									ORDER BY CSKUStates._Период DESC
 									) AS InitiativesTypes ON InitiativesTypes.InitiativesType = UID_1C
 
 				) AS InitiativesTypeID
-				,RegSales.Количество AS QuantityBase					-- Количество в базовых единицах измерения
-				,RegSales.Количество * ISNULL ((
+				,ISISTable.Volume	* ISNULL ((
+												SELECT TOP 1
+													ISNULL(MeasuresUnit.Коэффициент, 0)
+												FROM [uas_central].dbo.Справочник_ЕдиницыИзмерения AS MeasuresUnit
+												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON Class.Ссылка = MeasuresBase.БазоваяЕдиница
+												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0) AS QuantityBase					-- Количество в базовых единицах измерения
+				,ISISTable.Volume	* ISNULL ((
+												SELECT TOP 1
+													ISNULL(MeasuresUnit.Коэффициент, 0)
+												FROM [uas_central].dbo.Справочник_ЕдиницыИзмерения AS MeasuresUnit
+												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON Class.Ссылка = MeasuresBase.БазоваяЕдиница
+												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0)
+									* ISNULL ((
 												SELECT TOP 1
 													CASE
 														WHEN ISNULL(MeasuresUnit.Коэффициент, 0) = 0 THEN 0
@@ -77,17 +88,14 @@ BEGIN
 												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON MeasuresUnit.БазоваяЕдиница = Class.Ссылка
 																													AND Class.Код = '796'	-- [шт]
 												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0) AS QuantityUnit						-- Количество в [шт]
-				,RegSales.Количество * ISNULL ((
+				,ISISTable.Volume AS QuantityBox						-- Количество в [кор]
+				,ISISTable.Volume	* ISNULL ((
 												SELECT TOP 1
-													CASE
-														WHEN ISNULL(MeasuresUnit.Коэффициент, 0) = 0 THEN 0
-														ELSE 1 / MeasuresUnit.Коэффициент
-													END
-												FROM [uas_central].dbo.Справочник_ЕдиницыИзмерения AS MeasuresUnit	-- Справочник.ЕдиницыИзмерения
-												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON MeasuresUnit.БазоваяЕдиница = Class.Ссылка
-																													AND Class.Код = '384'	-- [кор]
-												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0) AS QuantityBox						-- Количество в [кор]
-				,RegSales.Количество * ISNULL ((
+													ISNULL(MeasuresUnit.Коэффициент, 0)
+												FROM [uas_central].dbo.Справочник_ЕдиницыИзмерения AS MeasuresUnit
+												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON Class.Ссылка = MeasuresBase.БазоваяЕдиница
+												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0)
+									* ISNULL ((
 												SELECT TOP 1
 													CASE
 														WHEN ISNULL(MeasuresUnit.Коэффициент, 0) = 0 THEN 0
@@ -97,7 +105,13 @@ BEGIN
 												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON MeasuresUnit.БазоваяЕдиница = Class.Ссылка
 																													AND Class.Код = '888'-- [уп]
 												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0) AS QuantityPack						-- Количество в [уп]
-				,RegSales.Количество * ISNULL ((
+				,ISISTable.Volume	* ISNULL ((
+												SELECT TOP 1
+													ISNULL(MeasuresUnit.Коэффициент, 0)
+												FROM [uas_central].dbo.Справочник_ЕдиницыИзмерения AS MeasuresUnit
+												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON Class.Ссылка = MeasuresBase.БазоваяЕдиница
+												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0)
+									* ISNULL ((
 												SELECT TOP 1
 													CASE
 														WHEN ISNULL(MeasuresUnit.Коэффициент, 0) = 0 THEN 0
@@ -107,56 +121,85 @@ BEGIN
 												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON MeasuresUnit.БазоваяЕдиница = Class.Ссылка
 																													AND Class.Код = '384'	-- [кор]
 												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0) * Goods.MSU / 1000 AS QuantityMSU	-- Количество в [MSU]
-				,ISNULL(MeasuresBase.Объем, 0) * RegSales.Количество AS Value					-- Объем
-				,ISNULL(MeasuresBase.ВесБрутто, 0) * RegSales.Количество AS WeightGross			-- Вес брутто
-				,ISNULL(MeasuresBase.ВесНетто, 0) * RegSales.Количество AS WeightNet			-- Вес брутто
-				,RegSales.СуммаНДС AS AmountVAT													-- Сумма НДС
-				,RegSales.СуммаБезСкидки AS AmountWithoutDiscount								-- Сумма без скидки
-				,RegSales.Сумма AS Amount														-- Сумма
-				,ISNULL(	(	SELECT TOP 1
-									Cost.Сумма AS Ammount
-								FROM [uas_central].dbo.РегистрСведений_Срезы AS Cost
-								WHERE Cost.Активность = 0x01
-									AND Cost.Номенклатура = RegSales.Номенклатура
-									AND Cost._Период <= RegSales._Период
-								ORDER BY Cost._Период DESC)
-						, 0) * RegSales.Количество AS AmountInCost						-- Сумма себестоимости
-				,ISNULL(	(	SELECT TOP 1
-										Price.Цена AS InputPrice
-								FROM [uas_central].dbo.РегистрСведений_ЦеныНоменклатуры AS Price
-								INNER JOIN [uas_central].dbo.Справочник_ТипыЦенНоменклатуры AS PriceType ON PriceType.Ссылка = Price.ТипЦены
-																								AND PriceType.ИмяПредопределенныхДанных = 0x9688769FCF7C991545B04E324B1591F5
-								WHERE Price.Номенклатура = RegSales.Номенклатура
-									AND Price._Период <= RegSales._Период
-								ORDER BY Price._Период DESC)
-						, 0) * RegSales.Количество AS AmountInInputPrices				-- Сумма во входных ценах
-			FROM [uas_central].dbo.РегистрНакопления_Продажи AS RegSales
-			INNER JOIN dbo.t_Documents AS Documents ON Documents.UID_1C = RegSales.Регистратор
-			LEFT JOIN dbo.t_SalesDocuments AS SalesDocuments ON SalesDocuments.UID_1C = RegSales.ДокументПродажи_Ссылка
-			LEFT JOIN dbo.t_TransactionsType AS TransactionsType ON TransactionsType.UID_1C = RegSales.ХозяйственнаяОперация
-			LEFT JOIN dbo.t_Routes AS Routes_ ON (Routes_.UID_1C = RegSales.Маршрут AND ISNULL(RegSales.Маршрут, 0x00) <> 0x00)
-												OR (Routes_.ID = SalesDocuments.RouteID AND ISNULL(RegSales.Маршрут, 0x00) = 0x00)
+				,ISISTable.Volume	* ISNULL ((
+												SELECT TOP 1
+													ISNULL(MeasuresUnit.Коэффициент, 0)
+												FROM [uas_central].dbo.Справочник_ЕдиницыИзмерения AS MeasuresUnit
+												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON Class.Ссылка = MeasuresBase.БазоваяЕдиница
+												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0)
+									* ISNULL(MeasuresBase.Объем, 0) AS Value					-- Объем
+				,ISISTable.Volume	* ISNULL ((
+												SELECT TOP 1
+													ISNULL(MeasuresUnit.Коэффициент, 0)
+												FROM [uas_central].dbo.Справочник_ЕдиницыИзмерения AS MeasuresUnit
+												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON Class.Ссылка = MeasuresBase.БазоваяЕдиница
+												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0)
+									* ISNULL(MeasuresBase.ВесБрутто, 0) AS WeightGross			-- Вес брутто
+				,ISISTable.Volume	* ISNULL ((
+												SELECT TOP 1
+													ISNULL(MeasuresUnit.Коэффициент, 0)
+												FROM [uas_central].dbo.Справочник_ЕдиницыИзмерения AS MeasuresUnit
+												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON Class.Ссылка = MeasuresBase.БазоваяЕдиница
+												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0)
+									* ISNULL(MeasuresBase.ВесНетто, 0) AS WeightNet				-- Вес нетто
+				,0 AS AmountVAT																	-- Сумма НДС
+				,ISISTable.GIV AS AmountWithoutDiscount											-- Сумма без скидки
+				,ISISTable.NIV AS Amount														-- Сумма
+				,ISISTable.Volume	* ISNULL ((
+												SELECT TOP 1
+													ISNULL(MeasuresUnit.Коэффициент, 0)
+												FROM [uas_central].dbo.Справочник_ЕдиницыИзмерения AS MeasuresUnit
+												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON Class.Ссылка = MeasuresBase.БазоваяЕдиница
+												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0)
+									* ISNULL ((	
+												SELECT TOP 1
+													Cost.Сумма AS Ammount
+												FROM [uas_central].dbo.РегистрСведений_Срезы AS Cost
+												WHERE Cost.Активность = 0x01
+													AND Cost.Номенклатура = ISISTable._tovar_id_new
+													AND Cost._Период <= CAST(ISISTable.DUE_PERD AS date)
+												ORDER BY Cost._Период DESC)
+											, 0) AS AmountInCost								-- Сумма себестоимости
+				,ISISTable.Volume	* ISNULL ((
+												SELECT TOP 1
+													ISNULL(MeasuresUnit.Коэффициент, 0)
+												FROM [uas_central].dbo.Справочник_ЕдиницыИзмерения AS MeasuresUnit
+												INNER JOIN [uas_central].dbo.Справочник_КлассификаторЕдиницИзмерения AS Class ON Class.Ссылка = MeasuresBase.БазоваяЕдиница
+												WHERE MeasuresUnit.Владелец = Goods.UID_1C), 0)
+									* ISNULL ((
+												SELECT TOP 1
+														Price.Цена AS InputPrice
+												FROM [uas_central].dbo.РегистрСведений_ЦеныНоменклатуры AS Price
+												INNER JOIN [uas_central].dbo.Справочник_ТипыЦенНоменклатуры AS PriceType ON PriceType.Ссылка = Price.ТипЦены
+																												AND PriceType.ИмяПредопределенныхДанных = 0x9688769FCF7C991545B04E324B1591F5
+												WHERE Price.Номенклатура = ISISTable._tovar_id_new
+													AND Price._Период <= CAST(ISISTable.DUE_PERD AS date)
+												ORDER BY Price._Период DESC)
+											, 0) AS AmountInInputPrices							-- Сумма во входных ценах
+			FROM [pg_reports].dbo.isis_work_table AS ISISTable
+			INNER JOIN dbo.t_Documents AS Documents ON Documents.UID_1C = ISISTable._iddoc
+			LEFT JOIN dbo.t_SalesDocuments AS SalesDocuments ON SalesDocuments.UID_1C = ISISTable._rashod_iddoc
+			LEFT JOIN [uas_central].dbo.Документ_РеализацияТоваров AS SalesDoc_1C ON SalesDoc_1C.Ссылка = ISISTable._rashod_iddoc
+			LEFT JOIN dbo.t_TransactionsType AS TransactionsType ON TransactionsType.UID_1C = IIF(ISISTable.Volume >= 0, 0xAFFBA27C585D82A441142E9FD4A14101, 0xAC3B38E4640B29B34E8A850E3E15B6D4)
+			LEFT JOIN dbo.t_Routes AS Routes_ ON Routes_.ID = SalesDocuments.RouteID
 			LEFT JOIN dbo.t_Teams AS Teams ON Teams.ID = Routes_.TeamID
-			LEFT JOIN dbo.t_Staff AS Staff ON (Staff.UID_1C = RegSales.ТорговыйАгент AND ISNULL(RegSales.ТорговыйАгент, 0x00) <> 0x00)
-											OR (Staff.ID = SalesDocuments.StaffID AND ISNULL(RegSales.ТорговыйАгент, 0x00) = 0x00)
-			LEFT JOIN dbo.t_PriceTypes AS PriceTypes ON (PriceTypes.UID_1C = RegSales.ТипЦены AND ISNULL(RegSales.ТипЦены, 0x00) <> 0x00)
-													OR (PriceTypes.ID = SalesDocuments.PriceTypeID AND ISNULL(RegSales.ТипЦены, 0x00) = 0x00)
-			LEFT JOIN dbo.t_Business AS Business ON Business.UID_1C = RegSales.НаправлениеБизнеса
-			LEFT JOIN dbo.t_Organizations AS Organizations ON Organizations.UID_1C = RegSales.Организация
-			LEFT JOIN dbo.t_Branches AS Branches ON Branches.UID_1C = RegSales.Филиал
-			LEFT JOIN dbo.t_Storehouses AS Storehouses ON Storehouses.UID_1C = RegSales.Склад
-			LEFT JOIN dbo.t_Customers AS Customers ON Customers.UID_1C = RegSales.Контрагент
-			LEFT JOIN dbo.t_DeliveryPoints AS DeliveryPoints ON DeliveryPoints.UID_1C = RegSales.ТочкаДоставки
-			LEFT JOIN dbo.t_CreditLines AS CreditLines ON CreditLines.UID_1C = RegSales.КредитноеНаправление
-			LEFT JOIN dbo.t_Goods AS Goods ON Goods.UID_1C = RegSales.Номенклатура
+			LEFT JOIN dbo.t_Staff AS Staff ON Staff.ID = SalesDocuments.StaffID
+			LEFT JOIN dbo.t_PriceTypes AS PriceTypes ON PriceTypes.ID = SalesDocuments.PriceTypeID
+			LEFT JOIN dbo.t_Business AS Business ON Business.UID_1C = SalesDoc_1C.НаправлениеБизнеса
+			LEFT JOIN dbo.t_Organizations AS Organizations ON Organizations.UID_1C = SalesDoc_1C.Организация
+			LEFT JOIN dbo.t_Branches AS Branches ON Branches.UID_1C = SalesDoc_1C.Филиал
+			LEFT JOIN dbo.t_Storehouses AS Storehouses ON Storehouses.UID_1C = SalesDoc_1C.Склад
+			LEFT JOIN dbo.t_DeliveryPoints AS DeliveryPoints ON DeliveryPoints.UID_1C = ISISTable._client_id_new
+			LEFT JOIN dbo.t_Customers AS Customers ON Customers.UID_1C = (SELECT TOP 1 Владелец FROM [uas_central].dbo.Справочник_ТочкиДоставки WHERE Ссылка = ISISTable._client_id_new)
+			LEFT JOIN dbo.t_CreditLines AS CreditLines ON CreditLines.UID_1C = SalesDoc_1C.КредитноеНаправление
+			LEFT JOIN dbo.t_Goods AS Goods ON Goods.UID_1C = ISISTable._tovar_id_new
 			LEFT JOIN [uas_central].dbo.Справочник_ЕдиницыИзмерения AS MeasuresBase ON MeasuresBase.Ссылка = Goods.MeasuresBaseUID_1C AND MeasuresBase.ПометкаУдаления = 0
-			WHERE RegSales.Активность = 0x01
-				AND CAST(RegSales._Период AS date) BETWEEN @StartDate AND @EndDate
+			WHERE CAST(ISISTable.DUE_PERD AS date) BETWEEN @StartDate AND @EndDate
 			) AS From_1C
 	ON ReportingTable.DocumentID = From_1C.DocumentID AND ReportingTable.DocumentRow = From_1C.DocumentRow
 		WHEN MATCHED THEN
 			UPDATE
-			SET	TransactionDate = From_1C.TransactionDate
+			SET	TransactionDateID = From_1C.TransactionDateID
 				,SalesDocumentID = From_1C.SalesDocumentID
 				,TransactionTypeID = From_1C.TransactionTypeID
 				,BusinessID = From_1C.BusinessID
@@ -185,10 +228,11 @@ BEGIN
 				,Amount = From_1C.Amount
 				,AmountInCost = From_1C.AmountInCost
 				,AmountInInputPrices = From_1C.AmountInInputPrices
-		WHEN NOT MATCHED BY SOURCE AND ReportingTable.TransactionDate BETWEEN @StartDate AND @EndDate THEN
+		WHEN NOT MATCHED BY SOURCE AND (ReportingTable.TransactionDateID >= CAST(CONVERT(varchar(8), @StartDate ,112) AS int)
+										AND ReportingTable.TransactionDateID <= CAST(CONVERT(varchar(8), @EndDate ,112) AS int)) THEN
 			DELETE
 		WHEN NOT MATCHED BY TARGET THEN
-			INSERT (	TransactionDate
+			INSERT (	TransactionDateID
 						,DocumentID
 						,DocumentRow
 						,SalesDocumentID
@@ -219,7 +263,7 @@ BEGIN
 						,Amount
 						,AmountInCost
 						,AmountInInputPrices)
-			VALUES (	From_1C.TransactionDate
+			VALUES (	From_1C.TransactionDateID
 						,From_1C.DocumentID
 						,From_1C.DocumentRow
 						,From_1C.SalesDocumentID
